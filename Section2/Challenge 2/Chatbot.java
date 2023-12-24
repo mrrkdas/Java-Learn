cb15ea1b23e04dc48b07713e1ccdaa28bd5a0f36
Rishab Das <das.rishabk@gmail.com>
2023-12-24 14:17:25 -0500

    finished section 2

--- Section2/Challenge 2/Chatbot.java
+++ Section2/Challenge 2/Chatbot.java
@@ -1,4 +1,5 @@
 //import Scanner
+import java.util.Scanner;
 
 public class Chatbot {
     public static void main(String[] args) {
@@ -7,33 +8,41 @@ public class Chatbot {
 
         
         //set up scanner. 
+        Scanner scanner = new Scanner(System.in);
+
 
         System.out.println("Hello. What is your name?");
         //Pick up user's name and store it. 
+        String name = scanner.nextLine();
 
                            
         //add new a line before asking next question. 
-        System.out.println("Hi <name>! I'm Javabot. Where are you from?");
+        System.out.println("\nHi" +name+ " I'm Javabot. Where are you from?");
         //Pick up user's home and store it. 
+        String home = scanner.nextLine();
                         
 
         //add new a line before asking next question.
-        System.out.println("I hear it's beautiful at <home>! I'm from a place called Oracle");
-        System.out.println("How old are you?");
+        System.out.println("I hear it's beautiful at "+home+"! I'm from a place called Oracle");
+        System.out.println("\nHow old are you?");
         //Pick up age and store it.
+        int age = scanner.nextInt();
 
 
         //add new a line before asking next question.
-        System.out.println("So you're <age>, cool! I'm 400 years old.");
-        System.out.println("This means I'm <divide 400 by age> times older than you.");
+        System.out.println("So you're " +age+", cool! I'm 400 years old.");
+        System.out.println("\nThis means I'm " + ((double) age/400) + "times older than you."); // SHOULD JUST BE 400/AGE
         System.out.println("Enough about me. What's your favourite language? (just don't say Python)");
         //Pick up language and store it. 
+        // nextLine Trap is here, the int has white space after it and the nextLine will pick up the white space, need to add another nextLine()
+        String language = scanner.nextLine();
 
 
         //add new a line here.
-        System.out.println("<language>, that's great! Nice chatting with you <name>. I have to log off now. See ya!");
+        System.out.println(language + ", that's great! Nice chatting with you " +name+". I have to log off now. See ya!");
         
         //close scanner. 
+        scanner.close();
 
         
     }
