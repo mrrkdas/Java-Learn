2669aaa26e59fbdbac36dc4bcb152c3fa71ab8f1
Rishab Das <das.rishabk@gmail.com>
2024-01-01 14:00:35 -0500

    commit

--- Section 7/workbooks/Workbook 7.1/Main.java
+++ Section 7/workbooks/Workbook 7.1/Main.java
@@ -1,3 +1,10 @@
 public class Main {
-    
+    Person person = new Person(null, null, null, 0);
+
+    person.name =  "Rishab";
+    person.nationality = "USA";
+    person.dateOfBirth = "02/03/2024";
+    person.passport = {person.name, person.nationality, person.dateOfBirth};
+    person.seatNumber = // an Integer
+
 }
